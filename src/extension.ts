e5dac5a7013121488f93ca812148a00fe6c710b8
Jeff Chen <jeff.chen@axleinfo.com>
2023-10-25 12:53:04 -0400

    Removed unused import

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
-import { InputBoxOptions } from "vscode";
 import { URL, Path, PolusArgs, Polus } from "./polus-render";
 import {exec} from "child_process";
 
